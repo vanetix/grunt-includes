2b50ee0cfbf3172a17035c4059151e0c6f181604
Webdevotion <Webdevotion>
2015-03-25 15:41:01 +0100

    do not pass in filenameSuffix specified in the Gruntfile when a suffix has been given by the include statement

--- tasks/includes.js
+++ tasks/includes.js
@@ -195,6 +195,15 @@ module.exports = function(grunt) {
           indent = '';
         }
 
+        // do not provide a filenameSuffix if one has been given by the include
+        // this allows the end user to specify a suffix to overwrite the default
+        // given in the gruntfile in special cases
+        var suffix = opts.filenameSuffix;
+        if( fileLocation.split('.').length > 1 )
+        {
+          suffix = '';
+        }
+
         fileLocation = opts.filenamePrefix + fileLocation + opts.filenameSuffix;
         next = path.join((opts.includePath || path.dirname(p)), fileLocation);
         content = recurse(next, opts, included, indents + indent);
